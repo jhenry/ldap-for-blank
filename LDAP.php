e86b55e3b94f2a50a87f7fd239816880593e20e2
Justin Henry <justin.henry@gmail.com>
2020-02-19 11:41:19 -0500

    Formatting.

--- LDAP.php
+++ LDAP.php
@@ -1,39 +1,39 @@
 <?php
-
 class LDAP extends PluginAbstract
 {
 	/**
-	* @var string Name of plugin
-	*/
+	 * @var string Name of plugin
+	 */
 	public $name = 'LDAP';
 
 	/**
-	* @var string Description of plugin
-	*/
+	 * @var string Description of plugin
+	 */
 	public $description = 'Provides directory lookup and query tools for LDAP.';
 
 	/**
-	* @var string Name of plugin author
-	*/
+	 * @var string Name of plugin author
+	 */
 	public $author = 'Justin Henry';
 
 	/**
-	* @var string URL to plugin's website
-	*/
+	 * @var string URL to plugin's website
+	 */
 	public $url = 'https://uvm.edu/~jhenry/';
 
 	/**
-	* @var string Current version of plugin
-	*/
+	 * @var string Current version of plugin
+	 */
 	public $version = '0.0.3';
-	
+
 	/**
-	* The plugin's gateway into codebase. Place plugin hook attachments here.
-	*/	
-	public function load(){
+	 * The plugin's gateway into codebase. Place plugin hook attachments here.
+	 */
+	public function load()
+	{
 		// Nothing to load.
 	}
-	
+
 
 	/**
 	 * Lookup a user in the directory.
@@ -41,8 +41,9 @@ class LDAP extends PluginAbstract
 	 * @param string $username
 	 * @return array 
 	 */
-	public function get_entry( $username ) {
-		return LDAP::flatten_ldap_arrays( LDAP::get_ldap_entry( $username ) );		
+	public function get_entry($username)
+	{
+		return LDAP::flatten_ldap_arrays(LDAP::get_ldap_entry($username));
 	}
 
 	/**
@@ -51,10 +52,11 @@ class LDAP extends PluginAbstract
 	 * @param array $ldap_array
 	 * @return array
 	 */
-	public function flatten_ldap_arrays($ldap_array) {
-		foreach ( $ldap_array as &$item ) {
-			if ( is_array($item)) {
-				$item = implode( ";", $item);
+	public function flatten_ldap_arrays($ldap_array)
+	{
+		foreach ($ldap_array as &$item) {
+			if (is_array($item)) {
+				$item = implode(";", $item);
 			}
 		}
 		unset($item);
@@ -67,23 +69,24 @@ class LDAP extends PluginAbstract
 	 * @param int $username
 	 * @return array
 	 */
-	public function get_ldap_entry($username) {
+	public function get_ldap_entry($username)
+	{
 
 		$filter = "netid=" . $username;
-			
+
 		// Connection and Base DN string configuration.
 		$ldapserver = "ldaps://ldap.uvm.edu";
 		$dnstring = "dc=uvm,dc=edu";
-		
+
 		$ds = ldap_connect($ldapserver);
-		
+
 		if ($ds) {
 			// Bind (anonymously, no auth) and search
 			$r = ldap_bind($ds);
 			$sr = ldap_search($ds, $dnstring, $filter);
 
 			// Kick out on a failed search.
-			if(!ldap_count_entries($ds, $sr))
+			if (!ldap_count_entries($ds, $sr))
 				return false;
 
 			// Retrieve records found by the search
@@ -97,7 +100,6 @@ class LDAP extends PluginAbstract
 			//return $attrs;
 			return LDAP::cleanUpEntry($attrs);
 		}
-
 	}
 
 	/**
@@ -107,23 +109,24 @@ class LDAP extends PluginAbstract
 	 * @param array $entry
 	 * @return array
 	 */
-	public function cleanUpEntry( $entry ) {
+	public function cleanUpEntry($entry)
+	{
 		$retEntry = array();
-		for ( $i = 0; $i < $entry['count']; $i++ ) {
+		for ($i = 0; $i < $entry['count']; $i++) {
 			if (is_array($entry[$i])) {
 				$subtree = $entry[$i];
-				if ( ! empty($subtree['dn']) and ! isset($retEntry[$subtree['dn']])) {
+				if (!empty($subtree['dn']) and !isset($retEntry[$subtree['dn']])) {
 					$retEntry[$subtree['dn']] = cleanUpEntry($subtree);
 				} else {
 					$retEntry[] = cleanUpEntry($subtree);
 				}
 			} else {
 				$attribute = $entry[$i];
-				if ( $entry[$attribute]['count'] == 1 ) {
+				if ($entry[$attribute]['count'] == 1) {
 					$retEntry[$attribute] = $entry[$attribute][0];
 				} else {
-					for ( $j = 0; $j < $entry[$attribute]['count']; $j++ ) {
-					  $retEntry[$attribute][] = $entry[$attribute][$j];
+					for ($j = 0; $j < $entry[$attribute]['count']; $j++) {
+						$retEntry[$attribute][] = $entry[$attribute][$j];
 					}
 				}
 			}
@@ -131,4 +134,3 @@ class LDAP extends PluginAbstract
 		return $retEntry;
 	}
 }
-
