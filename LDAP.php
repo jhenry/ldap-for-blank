6537553b6c1c822198d3cee31ef0cbf5d4014027
Justin Henry <justin.henry@gmail.com>
2020-02-20 06:40:14 -0500

    Filtered attribute based on default setting values

--- LDAP.php
+++ LDAP.php
@@ -1,164 +1,184 @@
 <?php
 class LDAP extends PluginAbstract
 {
-	/**
-	 * @var string Name of plugin
-	 */
-	public $name = 'LDAP';
+  /**
+   * @var string Name of plugin
+   */
+  public $name = 'LDAP';
 
-	/**
-	 * @var string Description of plugin
-	 */
-	public $description = 'Provides directory lookup and query tools for LDAP.';
+  /**
+   * @var string Description of plugin
+   */
+  public $description = 'Provides directory lookup and query tools for LDAP.';
 
-	/**
-	 * @var string Name of plugin author
-	 */
-	public $author = 'Justin Henry';
+  /**
+   * @var string Name of plugin author
+   */
+  public $author = 'Justin Henry';
 
-	/**
-	 * @var string URL to plugin's website
-	 */
-	public $url = 'https://uvm.edu/~jhenry/';
+  /**
+   * @var string URL to plugin's website
+   */
+  public $url = 'https://uvm.edu/~jhenry/';
 
-	/**
-	 * @var string Current version of plugin
-	 */
-	public $version = '0.0.3';
+  /**
+   * @var string Current version of plugin
+   */
+  public $version = '0.0.3';
 
-	/**
-	 * The plugin's gateway into codebase. Place plugin hook attachments here.
-	 */
-	public function load()
-	{
-		// Nothing to load.
-	}
+  /**
+   * The plugin's gateway into codebase. Place plugin hook attachments here.
+   */
+  public function load()
+  {
+    // Nothing to load.
+  }
 
-	/**
-	 * Performs install operations for plugin. Called when user clicks install
-	 * plugin in admin panel.
-	 *
-	 */
-	public function install()
-	{
-		// Add default directory fields to Settings.
-		$attributes = array('homeDirectory', 'givenName', 'sn', 'ou', 'labeledURI', 'mail', 'eduPersonPrimaryAffiliation');
+  /**
+   * Performs install operations for plugin. Called when user clicks install
+   * plugin in admin panel.
+   *
+   */
+  public function install()
+  {
+    // Add default directory fields to Settings.
+    $attributes = array('homeDirectory', 'givenName', 'sn', 'ou', 'labeledURI', 'mail', 'eduPersonPrimaryAffiliation');
 
-		Settings::set('ldap_attributes', json_encode($attributes));
-		Settings::set('ldap_filter_prefix', 'netid');
-		Settings::set('ldap_uri', 'ldaps://ldap.uvm.edu');
-		Settings::set('ldap_dn_string', 'dc=uvm,dc=edu');
-	}
-	/**
-	 * Performs uninstall operations for plugin. Called when user clicks
-	 * uninstall plugin in admin panel and prior to files being removed.
-	 *
-	 */
-	public function uninstall()
-	{
-		Settings::remove('ldap_attributes');
-		Settings::remove('ldap_filter_prefix');
-		Settings::remove('ldap_uri');
-		Settings::remove('ldap_dn_string');
-	}
+    Settings::set('ldap_attributes', json_encode($attributes));
+    Settings::set('ldap_filter_prefix', 'netid');
+    Settings::set('ldap_uri', 'ldaps://ldap.uvm.edu');
+    Settings::set('ldap_dn_string', 'dc=uvm,dc=edu');
+  }
+  /**
+   * Performs uninstall operations for plugin. Called when user clicks
+   * uninstall plugin in admin panel and prior to files being removed.
+   *
+   */
+  public function uninstall()
+  {
+    Settings::remove('ldap_attributes');
+    Settings::remove('ldap_filter_prefix');
+    Settings::remove('ldap_uri');
+    Settings::remove('ldap_dn_string');
+  }
 
 
-	/**
-	 * Lookup a user in the directory.
-	 * 
-	 * @param string $username
-	 * @return array 
-	 */
-	public function get_entry($username)
-	{
-		return LDAP::flatten_ldap_arrays(LDAP::get_ldap_entry($username));
-	}
+  /**
+   * Lookup a user in the directory.
+   * 
+   * @param string $username ID of user to query in the directory
+   * @return bool Indicator for whether or not to only return the default attributes defined in settings. 
+   */
+  public function get($username, $defaults = true)
+  {
+    $directory_entry = LDAP::flatten_ldap_arrays(LDAP::directory_query($username));
+    if ($defaults) {
+      LDAP::filter_default_attributes($directory_entry);
+    } else {
+      return $directory_entry;
+    }
+  }
 
-	/**
-	 * Flatten/join arrays for entries with multiple items.
-	 * 
-	 * @param array $ldap_array
-	 * @return array
-	 */
-	public function flatten_ldap_arrays($ldap_array)
-	{
-		foreach ($ldap_array as &$item) {
-			if (is_array($item)) {
-				$item = implode(";", $item);
-			}
-		}
-		unset($item);
-		return $ldap_array;
-	}
+  /**
+   * Filter default attributes from raw directory query
+   * 
+   * @param array $ldap_array
+   * @return array
+   */
+  public function filter_default_attributes($entry)
+  {
+      foreach( Settings::get('ldap_attributes') as $attribute ) {
+        $filtered_entry[$attribute] = $entry[$attribute];
+      }
 
-	/**
-	 * Get an entry in LDAP for the specified username.
-	 * 
-	 * @param int $username
-	 * @return array
-	 */
-	public function get_ldap_entry($username)
-	{
-		$prefix = Settings::get('ldap_filter_prefix');
-		$filter = $prefix . "=" . $username;
+      return $filtered_entry;
+  }
 
-		// Connection and Base DN string configuration.
-		$ldapserver = Settings::get('ldap_uri');
-		$dnstring = Settings::get('ldap_dn_string');
+  /**
+   * Flatten/join arrays for entries with multiple items for easier traversing.
+   * 
+   * @param array $ldap_array raw directory query result
+   * @return array $ldap_array "flattened" array.
+   */
+  public function flatten_ldap_arrays($ldap_array)
+  {
+    foreach ($ldap_array as &$item) {
+      if (is_array($item)) {
+        $item = implode(";", $item);
+      }
+    }
+    unset($item);
+    return $ldap_array;
+  }
 
-		$ds = ldap_connect($ldapserver);
+  /**
+   * Get an entry in LDAP for the specified username.
+   * 
+   * @param int $username
+   * @return array
+   */
+  public function directory_query($username)
+  {
+    $prefix = Settings::get('ldap_filter_prefix');
+    $filter = $prefix . "=" . $username;
 
-		if ($ds) {
-			// Bind (anonymously, no auth) and search
-			$r = ldap_bind($ds);
-			$sr = ldap_search($ds, $dnstring, $filter);
+    // Connection and Base DN string configuration.
+    $ldapserver = Settings::get('ldap_uri');
+    $dnstring = Settings::get('ldap_dn_string');
 
-			// Kick out on a failed search.
-			if (!ldap_count_entries($ds, $sr))
-				return false;
+    $ds = ldap_connect($ldapserver);
 
-			// Retrieve records found by the search
-			$info = ldap_get_entries($ds, $sr);
-			$entry = ldap_first_entry($ds, $sr);
-			$attrs = ldap_get_attributes($ds, $entry);
+    if ($ds) {
+      // Bind (anonymously, no auth) and search
+      $r = ldap_bind($ds);
+      $sr = ldap_search($ds, $dnstring, $filter);
 
-			// Close the door on the way out.
-			ldap_close($ds);
+      // Kick out on a failed search.
+      if (!ldap_count_entries($ds, $sr))
+        return false;
 
-			//return $attrs;
-			return LDAP::cleanUpEntry($attrs);
-		}
-	}
+      // Retrieve records found by the search
+      $info = ldap_get_entries($ds, $sr);
+      $entry = ldap_first_entry($ds, $sr);
+      $attrs = ldap_get_attributes($ds, $entry);
 
-	/**
-	 * Clean up and flatten an LDAP entry array.  
-	 * https://secure.php.net/manual/en/function.ldap-get-entries.php#89508
-	 *
-	 * @param array $entry
-	 * @return array
-	 */
-	public function cleanUpEntry($entry)
-	{
-		$retEntry = array();
-		for ($i = 0; $i < $entry['count']; $i++) {
-			if (is_array($entry[$i])) {
-				$subtree = $entry[$i];
-				if (!empty($subtree['dn']) and !isset($retEntry[$subtree['dn']])) {
-					$retEntry[$subtree['dn']] = cleanUpEntry($subtree);
-				} else {
-					$retEntry[] = cleanUpEntry($subtree);
-				}
-			} else {
-				$attribute = $entry[$i];
-				if ($entry[$attribute]['count'] == 1) {
-					$retEntry[$attribute] = $entry[$attribute][0];
-				} else {
-					for ($j = 0; $j < $entry[$attribute]['count']; $j++) {
-						$retEntry[$attribute][] = $entry[$attribute][$j];
-					}
-				}
-			}
-		}
-		return $retEntry;
-	}
+      // Close the door on the way out.
+      ldap_close($ds);
+
+      //return $attrs;
+      return LDAP::cleanUpEntry($attrs);
+    }
+  }
+
+  /**
+   * Clean up and flatten an LDAP entry array.  
+   * https://secure.php.net/manual/en/function.ldap-get-entries.php#89508
+   *
+   * @param array $entry
+   * @return array
+   */
+  public function cleanUpEntry($entry)
+  {
+    $retEntry = array();
+    for ($i = 0; $i < $entry['count']; $i++) {
+      if (is_array($entry[$i])) {
+        $subtree = $entry[$i];
+        if (!empty($subtree['dn']) and !isset($retEntry[$subtree['dn']])) {
+          $retEntry[$subtree['dn']] = cleanUpEntry($subtree);
+        } else {
+          $retEntry[] = cleanUpEntry($subtree);
+        }
+      } else {
+        $attribute = $entry[$i];
+        if ($entry[$attribute]['count'] == 1) {
+          $retEntry[$attribute] = $entry[$attribute][0];
+        } else {
+          for ($j = 0; $j < $entry[$attribute]['count']; $j++) {
+            $retEntry[$attribute][] = $entry[$attribute][$j];
+          }
+        }
+      }
+    }
+    return $retEntry;
+  }
 }
